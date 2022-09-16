9659ee0520c0e7555587e33c7853c856167a7c28
Timofey X <vuki03@mail.ru>
2022-09-16 20:50:15 +0700

    Added dots in the end of javadoc first sentences

--- Task_1_1_1/src/main/java/ru/nsu/cjvth/heapsort/Main.java
+++ Task_1_1_1/src/main/java/ru/nsu/cjvth/heapsort/Main.java
@@ -3,13 +3,13 @@ package ru.nsu.cjvth.heapsort;
 import java.util.Scanner;
 
 /**
- * Contains main executable method
+ * Contains main executable method.
  */
 public class Main {
 
     /**
-     * Reads N - array size and N integers - its contents Sorts the array and prints every item on a
-     * new line
+     * Reads N - array size and N integers - its contents. Sorts the array and prints every item on
+     * a new line.
      *
      * @param args command line arguments, has no effect
      */
