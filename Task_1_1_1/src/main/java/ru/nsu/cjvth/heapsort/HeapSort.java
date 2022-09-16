f7c7ee27a44255620054535d2877656515c5e877
Timofey X <vuki03@mail.ru>
2022-09-16 20:19:36 +0700

    Completely rewrite HeapSort

---
+++ Task_1_1_1/src/main/java/ru/nsu/cjvth/heapsort/HeapSort.java
@@ -0,0 +1,31 @@
+package ru.nsu.cjvth.heapsort;
+
+public class HeapSort {
+
+    public static void heapSort(int[] a) {
+        for (int i = a.length / 2; i >= 0; i--) {
+            siftDown(a, i, a.length);
+        }
+        for (int i = a.length - 1; i > 0; i--) {
+            int t = a[0];
+            a[0] = a[i];
+            a[i] = t;
+            siftDown(a, 0, i);
+        }
+    }
+
+    private static void siftDown(int[] heapArray, int element, int length) {
+        for (int i = element, j = i * 2 + 1; j < length; i = j, j = i * 2 + 1) {
+            if (j + 1 < length && heapArray[j + 1] > heapArray[j]) {
+                j++;
+            }
+            if (heapArray[j] > heapArray[i]) {
+                int t = heapArray[i];
+                heapArray[i] = heapArray[j];
+                heapArray[j] = t;
+            } else {
+                break;
+            }
+        }
+    }
+}
