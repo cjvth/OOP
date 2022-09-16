c941f0624b9ea63a679a8fc399b8d1574f3c7dac
Timofey X <vuki03@mail.ru>
2022-09-16 20:45:23 +0700

    Added javadoc

--- Task_1_1_1/src/main/java/ru/nsu/cjvth/heapsort/Main.java
+++ Task_1_1_1/src/main/java/ru/nsu/cjvth/heapsort/Main.java
@@ -1,18 +1,26 @@
 package ru.nsu.cjvth.heapsort;
 
+import java.util.Scanner;
+
+/**
+ * Contains main executable method
+ */
 public class Main {
 
+    /**
+     * Reads N - array size and N integers - its contents Sorts the array and prints every item on a
+     * new line
+     *
+     * @param args command line arguments, has no effect
+     */
     public static void main(String[] args) {
-        //        Scanner sc = new Scanner(System.in);
-        //        int n = sc.nextInt();
-        //        int[] a = new int[n];
-        //        for (int i = 0; i < n; i++) {
-        //            a[i] = sc.nextInt();
-        //        }
-        //        System.out.println();
-
-        int n = 10;
-        int[] a = new int[]{5, 7, 2, 6, 4, 9, 1, 3, 8, 2};
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        System.out.println();
 
         HeapSort.heapSort(a);
         for (int i = 0; i < n; i++) {
