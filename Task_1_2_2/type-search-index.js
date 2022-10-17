6222a4a792b85e7ff75f9eeb7a00e5c42bf400f4
cjvth <cjvth@users.noreply.github.com>
2022-10-17 05:20:29 +0000

    Deploying to gh-pages from @ cjvth/OOP@0d875fc532cfdc7fadcd5f86032e827171e0351b 🚀

--- Task_1_2_2/type-search-index.js
+++ Task_1_2_2/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.cjvth","l":"Tree.SearchMethod"},{"p":"ru.nsu.cjvth","l":"Tree"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.cjvth","l":"Tree.IterationMethod"},{"p":"ru.nsu.cjvth","l":"Tree"}]
