62c30ecf28c582c1de38fafcd9124a4a41b42af6
Timofey X <vuki03@mail.ru>
2022-12-06 00:18:52 +0300

    Everything except Diploma and Grade Work

--- Task_1_4_1/src/main/java/ru/nsu/cjvth/gradebook/GradeBook.java
+++ Task_1_4_1/src/main/java/ru/nsu/cjvth/gradebook/GradeBook.java
@@ -81,7 +81,7 @@ public class GradeBook {
      *
      * @param semester number of the semester
      * @param title    name of the subject
-     * @throws IllegalArgumentException if such semester does not exist
+     * @throws IllegalArgumentException if semester number is illegal
      */
     public void removeSubject(int semester, String title) {
         if (isSemesterInvalid(semester)) {
@@ -94,52 +94,14 @@ public class GradeBook {
      * Get list of subjects in certain semester.
      *
      * @param semester number of the semester
-     * @return List of subject names
-     * @throws IllegalArgumentException if such semester does not exist
+     * @return set of subject names
+     * @throws IllegalArgumentException if semester number is illegal
      */
     public Set<String> getSubjects(int semester) {
         if (isSemesterInvalid(semester)) {
             throw new IllegalArgumentException("Semester number is invalid");
         }
         return subjects.get(semester - 1).keySet();
-    }
-
-    /**
-     * Put or change mark for previously added subject.
-     *
-     * @param semester number of the semester
-     * @param title    name of the subject
-     * @param mark     the mark, should be either 0 or 1 for {@link GradeType#BOOL_CREDIT} grade
-     *                 type or from 0 to 5 for {@link GradeType#DIFFERENTIATED_CREDIT} and
-     *                 {@link GradeType#EXAM}
-     * @throws IllegalArgumentException if subject with this title at this semester does not exist;
-     *                                  or if mark is out of bounds
-     */
-    public void setMark(int semester, String title, int mark) {
-
-    }
-
-    /**
-     * Remove mark for previously added subject. Does nothing if there is no mark.
-     *
-     * @param semester number of the semester
-     * @param title    name of the subject
-     * @throws IllegalArgumentException if subject with this title at this semester does not exist
-     */
-    public void unsetMark(int semester, String title) {
-
-    }
-
-    /**
-     * Remove mark for previously added subject. Does nothing if there is no mark.
-     *
-     * @param semester number of the semester
-     * @param title    name of the subject
-     * @return type of the grade from {@link GradeType}
-     * @throws IllegalArgumentException if subject with this title at this semester does not exist
-     */
-    public GradeType getGradeType(int semester, String title) {
-        return null;
     }
 
     /**
@@ -151,37 +113,192 @@ public class GradeBook {
      * @throws IllegalArgumentException if subject with this title at this semester does not exist
      */
     public Integer getMark(int semester, String title) {
-        return 5;
+        if (isSemesterInvalid(semester)) {
+            throw new IllegalArgumentException("Semester number is invalid");
+        }
+        if (!subjects.get(semester - 1).containsKey(title)) {
+            throw new IllegalArgumentException(
+                "Subject with this name does not exist in this semester");
+        }
+        Grade g = subjects.get(semester - 1).get(title);
+        if (g.hasMark) {
+            return g.mark;
+        } else {
+            return null;
+        }
     }
 
     /**
-     * Get mean mark value for certain semester.
+     * Put or change mark for previously added subject.
      *
      * @param semester number of the semester
-     * @return the mean mark of semester
-     * @throws IllegalArgumentException if no such semester
+     * @param title    name of the subject
+     * @param mark     the mark, should be either 0 or 1 for {@link GradeType#BOOL_CREDIT} grade
+     *                 type or from 0 to 5 for {@link GradeType#DIFFERENTIATED_CREDIT} and
+     *                 {@link GradeType#EXAM}
+     * @throws IllegalArgumentException if subject with this title at this semester does not exist;
+     *                                  or if mark is out of bounds for its grade type
      */
-    public double meanSemesterMark(int semester) {
-        return 4;
+    public void setMark(int semester, String title, int mark) {
+        if (isSemesterInvalid(semester)) {
+            throw new IllegalArgumentException("Semester number is invalid");
+        }
+        if (!subjects.get(semester - 1).containsKey(title)) {
+            throw new IllegalArgumentException(
+                "Subject with this name does not exist in this semester");
+        }
+        Grade g = subjects.get(semester - 1).get(title);
+        int highLimit = switch (g.gradeType) {
+            case BOOL_CREDIT -> 1;
+            case DIFFERENTIATED_CREDIT, EXAM -> 5;
+        };
+        if (mark < 0 || mark > highLimit) {
+            throw new IllegalArgumentException("Mark value is out of range for this type of grade");
+        }
+        g.hasMark = true;
+        g.mark = mark;
     }
 
     /**
-     * Get mean mark value across all semesters.
-     *
-     * @return the mean mark of all semesters
-     */
-    public double meanMark() {
-        return 3;
-    }
-
-    /**
-     * Get what stipend the student should be given in certain semester.
+     * Remove mark for previously added subject. Does nothing if there is no mark.
      *
      * @param semester number of the semester
+     * @param title    name of the subject
+     * @throws IllegalArgumentException if subject with this title at this semester does not exist
+     */
+    public void unsetMark(int semester, String title) {
+        if (isSemesterInvalid(semester)) {
+            throw new IllegalArgumentException("Semester number is invalid");
+        }
+        if (!subjects.get(semester - 1).containsKey(title)) {
+            throw new IllegalArgumentException(
+                "Subject with this name does not exist in this semester");
+        }
+        Grade g = subjects.get(semester - 1).get(title);
+        if (g.hasMark) {
+            g.hasMark = false;
+        }
+    }
+
+    /**
+     * Remove mark for previously added subject. Does nothing if there is no mark.
+     *
+     * @param semester number of the semester
+     * @param title    name of the subject
+     * @return type of the grade from {@link GradeType}
+     * @throws IllegalArgumentException if subject with this title at this semester does not exist
+     */
+    public GradeType getGradeType(int semester, String title) {
+        if (isSemesterInvalid(semester)) {
+            throw new IllegalArgumentException("Semester number is invalid");
+        }
+        if (!subjects.get(semester - 1).containsKey(title)) {
+            throw new IllegalArgumentException(
+                "Subject with this name does not exist in this semester");
+        }
+        return subjects.get(semester - 1).get(title).gradeType;
+    }
+
+    /**
+     * Get mean mark value for certain semester. Unset marks are ignored.
+     *
+     * @param semester number of the semester
+     * @return the mean mark of subjects with mark in the semester or null if no subjects have mark
+     * @throws IllegalArgumentException if semester number is illegal
+     */
+    public Double meanSemesterMark(int semester) {
+        if (isSemesterInvalid(semester)) {
+            throw new IllegalArgumentException("Semester number is invalid");
+        }
+        int total = 0;
+        int count = 0;
+        for (Grade g : subjects.get(semester - 1).values()) {
+            if (g.hasMark) {
+                count++;
+                if (g.gradeType == GradeType.BOOL_CREDIT) {
+                    total += g.mark * 5;
+                } else {
+                    total += g.mark;
+                }
+            }
+        }
+        if (count == 0) {
+            return null;
+        } else {
+            return (double) total / count;
+        }
+    }
+
+    /**
+     * Get mean mark value across all semesters. Unset marks are ignored.
+     *
+     * @return the mean mark of subjects with mark for all time or null if no subjects have mark
+     */
+    public Double meanMark() {
+        int total = 0;
+        int count = 0;
+        for (int sem = 0; sem < totalSemesters; sem++) {
+            for (Grade g : subjects.get(sem).values()) {
+                if (g.hasMark) {
+                    count++;
+                    if (g.gradeType == GradeType.BOOL_CREDIT) {
+                        total += g.mark * 5;
+                    } else {
+                        total += g.mark;
+                    }
+                }
+            }
+        }
+        if (count == 0) {
+            return null;
+        } else {
+            return (double) total / count;
+        }
+    }
+
+    /**
+     * Get what government academic stipend the student should be given in the next semester after
+     * the given one.
+     *
+     * @param semester number of the semester, based on which marks the student is going to get
+     *                 stipend in the next semester
      * @return 0 of none, 1 if normal, 2 if increased, -1 if not enough information to know
+     * @throws IllegalArgumentException if semester number is illegal
      */
     public int semesterStipend(int semester) {
-        return 0;
+        if (isSemesterInvalid(semester)) {
+            throw new IllegalArgumentException("Semester number is invalid");
+        }
+        boolean hasEmpty = false;
+        boolean has4 = false;
+        boolean has3 = false;
+        for (Grade g : subjects.get(semester - 1).values()) {
+            if (g.hasMark) {
+                if (g.gradeType == GradeType.BOOL_CREDIT) {
+                    if (g.mark == 0) {
+                        has3 = true;
+                    }
+                } else {
+                    if (g.mark < 5) {
+                        has4 = true;
+                        if (g.mark < 4) {
+                            has3 = true;
+                        }
+                    }
+                }
+            } else {
+                hasEmpty = true;
+            }
+        }
+        if (has3) {
+            return 0;
+        } else if (hasEmpty) {
+            return -1;
+        } else if (has4) {
+            return 1;
+        } else {
+            return 2;
+        }
     }
 
     /**
@@ -203,7 +320,7 @@ public class GradeBook {
         EXAM
     }
 
-    private class Grade {
+    private static class Grade {
 
         private final GradeType gradeType;
         private int mark;
