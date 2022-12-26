d6ba6746b57d199c6c07a050af5071e823ceb9ce
Timofey X <vuki03@mail.ru>
2022-12-26 22:40:02 +0700

    Converting from and to degrees, PI and E constants

--- Task_1_5_1/src/test/java/ru/nsu/cjvth/calculator/CalculatorTest.java
+++ Task_1_5_1/src/test/java/ru/nsu/cjvth/calculator/CalculatorTest.java
@@ -36,6 +36,7 @@ class CalculatorTest {
         assertDoesNotThrow(() -> Calculator.main(new String[]{":-("}));
         assertThrows(NoSuchElementException.class, () -> fast("+"));
         assertDoesNotThrow(() -> Calculator.main(new String[]{"+"}));
+        assertDoesNotThrow(() -> Calculator.main(new String[]{"+", "1", "2"}));
     }
 
     @Test
