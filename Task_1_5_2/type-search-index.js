713bcb964d4510834e948cae5eaf0e7a6470923c
cjvth <cjvth@users.noreply.github.com>
2022-12-28 20:51:50 +0000

    Deploying to gh-pages from @ cjvth/OOP@da64b8774257a5fb3847c64056dca3f7136da748 🚀

---
+++ Task_1_5_2/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.cjvth.notebook","l":"Main"},{"p":"ru.nsu.cjvth.notebook","l":"Notebook.Note"},{"p":"ru.nsu.cjvth.notebook","l":"Notebook"}]
