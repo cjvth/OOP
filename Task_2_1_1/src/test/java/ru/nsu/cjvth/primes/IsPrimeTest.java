3f93a56828c31db2c03ddd0c4f2a666e60d3ecbb
cjvth <vuki03@mail.ru>
2023-06-21 03:21:52 +0700

    Add SequentialRunner and tests

--- Task_2_1_1/src/test/java/ru/nsu/cjvth/primes/IsPrimeTest.java
+++ Task_2_1_1/src/test/java/ru/nsu/cjvth/primes/IsPrimeTest.java
@@ -7,23 +7,23 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-class CheckPrimeTest {
+class IsPrimeTest {
 
     @ParameterizedTest
     @ValueSource(ints = {-100, -3, -2, -1, 0, 1, 2})
     void isPrimeSpecial(int x) {
-        assertEquals(x > 1, CheckPrime.isPrime(x));
+        assertEquals(x > 1, IsPrime.isPrime(x));
     }
 
     @ParameterizedTest
     @ValueSource(ints = {3, 7, 19, 929, 8543, 1254863})
     void isPrimePrime(int x) {
-        assertTrue(CheckPrime.isPrime(x));
+        assertTrue(IsPrime.isPrime(x));
     }
 
     @ParameterizedTest
     @ValueSource(ints = {4, 6, 21, 343, 1854214})
     void isPrimeNonPrime(int x) {
-        assertFalse(CheckPrime.isPrime(x));
+        assertFalse(IsPrime.isPrime(x));
     }
 }
